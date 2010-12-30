9b50a399aa558abc25410fcd37580c8ddc1ebf6d
Juha Paananen <juha.paananen@reaktor.fi>
2010-12-30 12:05:26 +0200

    Start implementation of visualization client

---
+++ html/js/template.js
@@ -0,0 +1,22 @@
+if (typeof Template === 'undefined') {
+    var Template = {
+        renderElements : function(container, elementName, elements, renderFunction) {
+            var template = $('.template', container).first().clone();
+            resetTemplateContainer();
+            $.each(elements, function(index, item) {
+                Template.renderTemplate(item, renderFunction, template, container);
+            });
+
+            function resetTemplateContainer() {
+                $('.' + elementName + ':not(.template)', container).remove();
+            }
+        },
+
+        renderTemplate : function(item, itemTransformer, itemTemplate, itemsContainer) {
+            var itemElement = itemTemplate.clone();
+            itemElement.removeClass("template");
+            itemTransformer(item, itemElement);
+            itemsContainer.append(itemElement);
+        }
+    };
+}
