a22a2e8f34ed8bcedfe1c3045a6703a93a584d55
Antti Mattila <antti.mattila@reaktor.fi>
2011-01-26 18:40:37 +0200

    Allow connections from outside

--- src/httpserver.js
+++ src/httpserver.js
@@ -2,7 +2,7 @@ var http = http = require('http')
 
 exports.HttpServer = function(port, handler) {
     var server = http.createServer(handler);
-    server.listen(port, '127.0.0.1');
+    server.listen(port);
     console.log('Result server running on port ' + port);
     return server;
 }
